909a5b658373e617d1b75ec49dc90a213029356b
Nick Belov <nickbelov2003@gmail.com>
2025-12-14 15:40:03 -0500

    asd

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ import argparse
 import time
 from pathlib import Path
 
-from cgshop2026_pyutils.geometry import Point, compute_local_delaunay_flip_batches
+from cgshop2026_pyutils.geometry import (
+    Point,
+    FlippableTriangulation,
+    violates_local_delaunay as cgal_violates_local_delaunay,
+)
 from cgshop2026_pyutils.schemas import CGSHOP2026Instance, CGSHOP2026Solution
 from cgshop2026_pyutils.io import read_instance
 from cgshop2026_pyutils.verify import check_for_errors
@@ -41,12 +45,50 @@ def instance_points(instance: CGSHOP2026Instance) -> list[Point]:
     return [Point(x, y) for x, y in zip(instance.points_x, instance.points_y)]
 
 
+def build_triangulations(
+    instance: CGSHOP2026Instance, points: list[Point]
+) -> list[FlippableTriangulation]:
+    """Wrap every edge list of the instance in a FlippableTriangulation."""
+    return [
+        FlippableTriangulation.from_points_edges(points, edges)
+        for edges in instance.triangulations
+    ]
+
+
+def flip_to_delaunay(
+    triangulation: FlippableTriangulation, points: list[Point]
+) -> list[list[tuple[int, int]]]:
+    """Flip all non-Delaunay edges; returns batches of concurrently flipped edges."""
+    batches: list[list[tuple[int, int]]] = []
+    while True:
+        pending_batch: list[tuple[int, int]] = []
+        for edge in triangulation.possible_flips():
+            opposite = triangulation.get_flip_partner(edge)
+            a_idx, b_idx = edge
+            c_idx, d_idx = opposite
+            if cgal_violates_local_delaunay(
+                points[a_idx], points[b_idx], points[c_idx], points[d_idx]
+            ):
+                try:
+                    triangulation.add_flip(edge)
+                except ValueError:
+                    # Another flip in this batch now conflicts with this edge.
+                    continue
+                pending_batch.append(edge)
+        if not pending_batch:
+            break
+        triangulation.commit()
+        batches.append(pending_batch)
+    return batches
+
+
 def solve_instance(instance: CGSHOP2026Instance) -> CGSHOP2026Solution:
     """Return a CGSHOP2026Solution JSONable object."""
     points = instance_points(instance)
+    triangulations = build_triangulations(instance, points)
     return CGSHOP2026Solution(
         instance_uid=instance.instance_uid,
-        flips=compute_local_delaunay_flip_batches(points, instance.triangulations),
+        flips=[flip_to_delaunay(tri, points) for tri in triangulations],
         meta={"algorithm": "local_delaunay_flips"},
     )
 
