32e18a6e9a27cdac05f170c09337f911ed9ef15a
Nick Belov <nickbelov2003@gmail.com>
2025-12-15 20:31:08 -0500

    build .sh returns :skull:

---
+++ pyutils26/build.sh
@@ -0,0 +1,2 @@
+python3 -m pip install --no-cache-dir "conan>=2.0.0" "scikit-build>=0.17.3" "skbuild-conan" "cmake>=3.23" "ninja"
+python3 -m pip install --no-build-isolation --no-cache-dir -e pyutils26
