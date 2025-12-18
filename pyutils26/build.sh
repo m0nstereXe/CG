14bbed9eb8f5f8893736175936f9d41c9e98f978
Nick Belov <nickbelov2003@gmail.com>
2025-12-18 14:40:02 -0500

    fixed build and removed zip

--- pyutils26/build.sh
+++ pyutils26/build.sh
@@ -1,2 +1,3 @@
+source .venv/bin/activate
 python3 -m pip install --no-cache-dir "conan>=2.0.0" "scikit-build>=0.17.3" "skbuild-conan" "cmake>=3.23" "ninja"
 python3 -m pip install --no-build-isolation --no-cache-dir -e pyutils26
